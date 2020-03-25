571ecc00512b9b83cae0383791e317782a73cd1c
Adrian Halaburda <adh761@gmail.com>
2020-03-25 13:43:29 -0400

    Pluralizacion de las clases de Lotes para mejora de visualizacion en el admin

--- sse_django/apps/lotes/models.py
+++ sse_django/apps/lotes/models.py
@@ -7,6 +7,8 @@ class Lote(models.Model):
 	fecha_creacion = models.DateTimeField(auto_now_add=True, blank=True)
 	dependencia_destino_id = models.ForeignKey(Dependencia, null=False, blank=False, on_delete=models.CASCADE)
 	activo  = models.BooleanField(default=True)
+	class Meta:
+		verbose_name_plural = "Lotes"
 
 	def __str__(self):
 		return str(self.id)+" - "+str(self.descripcion)+"/"+str(self.dependencia_destino_id)
