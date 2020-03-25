154b5afa2207b24837984dcd01dc2c86fadf43c3
Adrian Halaburda <adh761@gmail.com>
2020-03-25 13:39:10 -0400

    Agregado a la vista del admin y correcion de pluralizacion

--- sse_django/apps/dependencias/admin.py
+++ sse_django/apps/dependencias/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Dependencia
+from .models import Dependencia_por_usuario
+
+admin.site.register(Dependencia)
+admin.site.register(Dependencia_por_usuario)
--- sse_django/apps/dependencias/models.py
+++ sse_django/apps/dependencias/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.conf import settings
 
 class Dependencia(models.Model):
 	descripcion = models.CharField(max_length=100)
 	activo  = models.BooleanField(default=True)
+
+	class Meta:
+		verbose_name_plural = "Dependencias"
+
 	def __str__(self):
 		return str(self.id)+" - "+str(self.descripcion)
 
 class Dependencia_por_usuario(models.Model):
 	dependencia_id = models.ForeignKey(Dependencia, null=True, blank=True, on_delete=models.CASCADE)
-	# usuario_id = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+	usuario_id = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
+
+	class Meta:
+		verbose_name_plural = "Dependencias por Usuarios"
 
 	def __str__(self):
 		return str(self.id)+" - "+str(self.dependencia_id)
