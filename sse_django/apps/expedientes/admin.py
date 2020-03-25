49253a33318401eea36391c89ccacd6f5676ebd0
Adrian Halaburda <adh761@gmail.com>
2020-03-25 13:40:28 -0400

    Visualizacion de expedientes desde el admin

--- sse_django/apps/expedientes/admin.py
+++ sse_django/apps/expedientes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Expediente
+from .models import Instancia
+from .models import Comentario
+from .models import Estado
+from .models import Prioridad
+from .models import Objeto_de_Gasto
+
+admin.site.register(Expediente)
+admin.site.register(Instancia)
+admin.site.register(Comentario)
+admin.site.register(Estado)
+admin.site.register(Prioridad)
+admin.site.register(Objeto_de_Gasto)
