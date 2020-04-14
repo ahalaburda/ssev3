458dcc84fd32262a6d9b12eeca4c3883eec5f878
Adrian Halaburda <adh761@gmail.com>
2020-04-14 08:42:10 -0400

    api: mejora de identacion segun Django y PEP8

--- backend/apps/expedientes/api/serializers.py
+++ backend/apps/expedientes/api/serializers.py
@@ -1,35 +1,44 @@
 from rest_framework import serializers
+
 from apps.expedientes.models import Expediente, Instancia, Estado, Comentario, Objeto_de_Gasto, Prioridad
 
+
 class Objeto_de_GastoSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Objeto_de_Gasto
         fields = '__all__'
 
 class ExpedienteSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Expediente
         # fields = ('descripcion', 'activo')
         fields = '__all__'
+        # depth = 1
 
 class InstanciaSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Instancia
         fields = '__all__'
+        # depth = 1
 
 class ComentarioSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Comentario
         fields = '__all__'
-
+        # depth = 2
 
 class PrioridadSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Prioridad
         fields = '__all__'
 
-
 class EstadoSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Estado
         fields = '__all__'
