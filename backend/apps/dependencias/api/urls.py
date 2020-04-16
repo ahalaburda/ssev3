59f2a2d931927811b5f3bb0b27a1767790329bc1
Angel <angel.marecos@fiuni.edu.py>
2020-04-16 11:18:45 -0400

    api: crud completado de dependencias, lotes y tipos de expedientes

--- backend/apps/dependencias/api/urls.py
+++ backend/apps/dependencias/api/urls.py
@@ -7,6 +7,6 @@ from .views import Dependencia_por_usuarioDetailView
 urlpatterns = [
     path('dependencias', DependenciaListView.as_view()),
     path('dependencias/<pk>', DependenciaDetailView.as_view()),
-    path('dependencias_por_usuarios/', Dependencia_por_usuarioListView.as_view()),
-    path('dependencias_por_usuarios/pk', Dependencia_por_usuarioDetailView.as_view())
+    path('dependencias_por_usuarios', Dependencia_por_usuarioListView.as_view()),
+    path('dependencias_por_usuarios/<pk>', Dependencia_por_usuarioDetailView.as_view())
 ]
--- backend/apps/tipos_de_expedientes/api/urls.py
+++ backend/apps/tipos_de_expedientes/api/urls.py
@@ -4,7 +4,7 @@ from .views import Tipo_de_expediente_detalleListView, Tipo_de_expediente_detall
 
 urlpatterns = [
     path('tipos_de_expedientes', Tipo_de_expedienteListView.as_view()),
-    path('tipos_de_expedientes/<int:pk>/', Tipo_de_expedienteDetailView.as_view()),
+    path('tipos_de_expedientes/<pk>', Tipo_de_expedienteDetailView.as_view()),
     path('tipos_de_expedientes_detalles', Tipo_de_expediente_detalleListView.as_view()),
-    path('tipos_de_expedientes_detalles/<int:pk>/', Tipo_de_expediente_detalleDetailView.as_view())
+    path('tipos_de_expedientes_detalles/<pk>', Tipo_de_expediente_detalleDetailView.as_view())
 ]
