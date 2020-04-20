11eddc1fb38f1b23e218a0077fbb55322f603409
Angel <angel.marecos@fiuni.edu.py>
2020-04-20 12:22:33 -0400

    api: busqueda por orden, tipo de expediente (fk) y dependencia por descripcion

--- backend/apps/tipos_de_expedientes/api/views.py
+++ backend/apps/tipos_de_expedientes/api/views.py
@@ -1,7 +1,10 @@
+from django_filters import rest_framework as filters
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from ..models import Tipo_de_expediente, Tipo_de_expediente_detalle
 from .serializers import Tipo_de_expedienteSerializer, Tipo_de_expediente_detalleSerializer, \
     Tipo_de_expediente_detalleNewUpdateSerializer
+
+
 
 
 class Tipo_de_expedienteListView(ListCreateAPIView):
@@ -13,10 +16,15 @@ class Tipo_de_expedienteDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Tipo_de_expediente.objects.all()
     serializer_class = Tipo_de_expedienteSerializer
 
+class Tipo_de_expediente_detalleFilter(filters.FilterSet):
+    class Meta:
+        model = Tipo_de_expediente_detalle
+        fields = ('tipo_de_expediente_id', 'orden', 'dependencia_id__descripcion')
 
 class Tipo_de_expediente_detalleListView(ListCreateAPIView):
     queryset = Tipo_de_expediente_detalle.objects.all()
-
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = Tipo_de_expediente_detalleFilter
     def get_serializer_class(self):
         if self.request.method in ['POST']:
             return Tipo_de_expediente_detalleNewUpdateSerializer
