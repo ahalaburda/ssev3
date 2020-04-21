eeb5a7432cd0afb77f90a1deb0c534329a4def03
Angel <angel.marecos@fiuni.edu.py>
2020-04-21 13:12:54 -0400

    api: filtro de lotes por descripcion, fecha de creacion y dependencia destino

--- backend/apps/lotes/api/views.py
+++ backend/apps/lotes/api/views.py
@@ -1,10 +1,19 @@
+from django_filters import rest_framework as filters
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from ..models import Lote
 from .serializers import LoteSerializer, LoteNewUpdateSerializer
 
+class LoteFilter(filters.FilterSet):
+    descripcion = filters.CharFilter(field_name='descripcion', lookup_expr='icontains')
+    fecha_creacion = filters.DateFromToRangeFilter(field_name='fecha_creacion')
+    class Meta:
+        model = Lote
+        fields = ('dependencia_destino_id', 'fecha_creacion', 'descripcion')
 
 class LoteListView(ListCreateAPIView):
     queryset = Lote.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = LoteFilter
 
     def get_serializer_class(self):
         if self.request.method in ['POST']:
