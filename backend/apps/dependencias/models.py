3bfa22612b22b2471caa6eb93e1fae009b91ebfb
Daniel <daniel545mjj@gmail.com>
2020-04-23 11:44:19 -0400

    api: validaciones desde el modelo

--- backend/apps/dependencias/models.py
+++ backend/apps/dependencias/models.py
@@ -14,8 +14,8 @@ class Dependencia(models.Model):
 
 
 class Dependencia_por_usuario(models.Model):
-    dependencia_id = models.ForeignKey(Dependencia, null=True, blank=True, on_delete=models.CASCADE)
-    usuario_id = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
+    dependencia_id = models.ForeignKey(Dependencia, null=False, blank=True, on_delete=models.CASCADE)
+    usuario_id = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, blank=True, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "Dependencias por Usuarios"
--- backend/apps/tipos_de_expedientes/models.py
+++ backend/apps/tipos_de_expedientes/models.py
@@ -4,7 +4,7 @@ from apps.dependencias.models import Dependencia
 
 
 class Tipo_de_expediente(models.Model):
-    descripcion = models.CharField(max_length=50)
+    descripcion = models.CharField(null=False, blank=False, max_length=50)
     activo = models.BooleanField(default=True)
 
     class Meta:
@@ -17,7 +17,7 @@ class Tipo_de_expediente(models.Model):
 class Tipo_de_expediente_detalle(models.Model):
     tipo_de_expediente_id = models.ForeignKey(Tipo_de_expediente, null=False, blank=False, on_delete=models.CASCADE)
     dependencia_id = models.ForeignKey(Dependencia, null=False, blank=False, on_delete=models.CASCADE)
-    orden = models.IntegerField(blank=True, null=True)
+    orden = models.IntegerField(blank=True, null=False)
 
     class Meta:
         verbose_name_plural = "Tipos de Expedientes Detalles / Rutas"
