0a144f86c2f23339a8444c96ba0e463d2249acb8
Angel <angel.marecos@fiuni.edu.py>
2020-04-29 11:30:39 -0400

    tipos_de_expediente: sobreescritura de metodos de de get y post

--- backend/apps/tipos_de_expedientes/api/views.py
+++ backend/apps/tipos_de_expedientes/api/views.py
@@ -3,13 +3,34 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from ..models import Tipo_de_expediente, Tipo_de_expediente_detalle
 from .serializers import Tipo_de_expedienteSerializer, Tipo_de_expediente_detalleSerializer, \
     Tipo_de_expediente_detalleNewUpdateSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 
 
 
-class Tipo_de_expedienteListView(ListCreateAPIView):
-    queryset = Tipo_de_expediente.objects.all()
-    serializer_class = Tipo_de_expedienteSerializer
+class Tipo_de_expedienteListView(APIView):
+
+    # Cache requested url for each user for 2 hours
+    @method_decorator(cache_page(60))
+    @method_decorator(vary_on_cookie)
+    def get(self, request, format=None):
+        tipo_de_expedientes = Tipo_de_expediente.objects.all()
+        serializer = Tipo_de_expedienteSerializer(tipo_de_expedientes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = Tipo_de_expedienteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class Tipo_de_expedienteDetailView(RetrieveUpdateDestroyAPIView):
