00235bade7688b51c9c07249c6ebbca94fc169ed
Adrian Halaburda <adh761@gmail.com>
2020-05-11 10:00:51 -0400

    fix:Correcion de nombre de ruta

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Footer.css'
+
+function Footer() {
+	return (
+		<footer className="sticky-footer bg-white">
+			<div className="container my-auto">
+				<div className="copyright text-center my-auto">
+					<span>Copyright &copy; Universidad Nacional de Itapua 2020</span>
+				</div>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
