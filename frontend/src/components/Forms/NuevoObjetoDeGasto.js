bb20ac3fc7a0f2441ff2f922a6db2961c92f9c4e
Angel <angel.marecos@fiuni.edu.py>
2020-06-25 11:34:36 -0400

    objeto de gasto: actualizacion de validaciones y comentarios

--- frontend/src/components/Forms/NuevoObjetoDeGasto.js
+++ frontend/src/components/Forms/NuevoObjetoDeGasto.js
@@ -1,34 +1,62 @@
 import React, {Component} from 'react';
 import ObjetosDeGastosService from '../../services/ObjetosDeGastos';
 import Popups from '../Popups';
+import SimpleReactValidator from "simple-react-validator";
+import {Modal} from "react-bootstrap";
 
+/**
+ * Modal para nuevo objeto de gasto
+ */
 class NuevoObjetoDeGasto extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            descripcion: ''
+            description: ''
         };
-
-        this.onChangeDescripcion = this.onChangeDescripcion.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    onChangeDescripcion(e) {
-        this.setState({
-            descripcion: e.target.value
+        this.setDescription = this.setDescription.bind(this);
+        this.handleSaveClick = this.handleSaveClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        //opciones y mensajes para la validacion
+        this.validator = new SimpleReactValidator({
+            className: 'text-danger',
+            messages: {
+                required: 'Este campo no puede estar vacío.'
+            }
         });
     }
 
-    handleSubmit() {
-        let data = {
-            descripcion: this.state.descripcion
-        };
+    /**
+     * Setea el estado para descripcion.
+     * @param e Evento del input
+     */
+    setDescription = e => {
+        this.setState({
+            description: e.target.value
+        });
+        this.checkValid();
+    }
 
-        ObjetosDeGastosService.create(data)
+    /**
+     * Si hay errores en los inputs, muestra los mensajes de error
+     */
+    checkValid = () => {
+        if (!this.validator.allValid()) this.validator.showMessages();
+    }
+
+    /**
+     * Guarda el nuevo objeto de gasto
+     */
+    save = () => {
+        ObjetosDeGastosService.create({descripcion: this.state.description})
             .then(response => {
                 if(response.status === 201){
-                    this.props.saveModalNew(response.data);
+                    this.props.newItem({
+                        id: response.data.id,
+                        descripcion: response.data.descripcion,
+                        activo: response.data.activo ? "Activo" : "Inactivo"
+                    });
                     Popups.success("Nuevo objeto de gasto creado con éxito");
+                    this.handleClose();
                 }
                 else {
                     Popups.error("Ocurrió un error, no se pudo crear");
@@ -39,42 +67,69 @@ class NuevoObjetoDeGasto extends Component {
             });
     }
 
+    /**
+     * Limpiar los campos cuando se cierra el modal.
+     */
+    handleClose = () => {
+        this.setState({
+            description: ''
+        });
+        this.validator.hideMessages();
+        this.props.setShow(false);
+    }
+
+    /**
+     * Si todas las validaciones son validas, entonces guarda, si no muestran los mensajes de errores
+     */
+    handleSaveClick = () => {
+        this.checkValid() & this.save() && this.props.setShow(false);
+    }
+
     render() {
         return (
-            <div className="modal fade" id="newModal" tabIndex="-1" role="dialog" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Nuevo Objeto de Gastos</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+            <Modal
+                show={this.props.showModal}
+                onHide={this.handleClose}
+                backdrop="static"
+                size="lg"
+                centered
+            >
+                <Modal.Header>
+                    <Modal.Title>Nuevo Objeto de Gasto</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <div className="form-group">
+                            <label>Descripcion *</label>
+                            <input
+                                type="text"
+                                className="form-control form-control-sm"
+                                name="description"
+                                placeholder="Agrega una descripcion"
+                                value={this.state.description}
+                                onChange={e => this.setDescription(e)}
+                                onBlur={e => this.setDescription(e)}
+                            />
+                            {this.validator.message('description', this.state.description, 'required')}
                         </div>
-                        <form>
-                            <div className="modal-body">
-
-                                <div className="form-group">
-                                    <label htmlFor="inputDescription">Descripción</label>
-                                    <input
-                                        type="text"
-                                        className="form-control form-control-sm"
-                                        value={this.state.descripcion}
-                                        onChange={this.onChangeDescripcion}
-                                        placeholder="Agrega una descripcion"/>
-                                </div>
-
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-sm btn-secondary" data-dismiss="modal">Cerrar
-                                </button>
-                                <button type="submit" className="btn btn-sm btn-primary" data-dismiss="modal"
-                                        onClick={() => this.handleSubmit()}>Guardar
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button
+                        type="button"
+                        className="btn btn-sm btn-secondary"
+                        onClick={this.handleClose}>
+                        Cerrar
+                    </button>
+                    <button
+                        onClick={this.handleSaveClick}
+                        type="button"
+                        className="btn btn-sm btn-primary"
+                    >
+                        Guardar
+                    </button>
+                </Modal.Footer>
+            </Modal>
         );
     }
 }
