b3016c577672f502ad93a00cf8532de4fef88b70
Daniel Velazquez <daniel545mjj@gmail.com>
2020-08-11 12:19:20 -0400

    frontend - consulta: busqueda por id (sin validaciones)

--- frontend/src/components/Tables/Consulta.js
+++ frontend/src/components/Tables/Consulta.js
@@ -1,56 +1,102 @@
-import React from "react";
+import React, {Component} from "react";
 import DataTable from "react-data-table-component";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-function Consulta(props) {
-  const data = [
-    {
-      id: 2,
-      numero: 3
-    },
-    {
-      id: 4,
-      numero: 3
-    },
-    {
-      id: 5,
-      numero: 3
+class Consulta extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
     }
-  ]
-  const columns = [
-    {
-      name: 'ID',
-      selector: 'id',
-      sortable: true,
-    },
-    {
-      name: 'Número',
-      selector: 'numero'
-    },
-    {
-      name: 'Acciones',
-      cell: () =>
-        <div>
-          <button className="btn btn-sm btn-link text-primary">
-            <FontAwesomeIcon icon="eye"/>
-          </button>
-        </div>,
-      button: true
-    }
-  ];
+  }
 
-  const paginationOptions = {
-    rowsPerPageText: 'Filas por página',
-    rangeSeparatorText: 'de',
-    selectAllRowsItem: true,
-    selectAllRowsItemText: 'Todos'
-  };
+  componentWillReceiveProps(nextProps, nextContext) {
+    this.setState({list: nextProps.data})
+  }
 
-  return (
-    <>
+  render() {
+    //fila temporal
+    const temp_data = [
+      {
+        id: '12343',
+        numero: '3324',
+        fechaMe: '31-12-2020 11:26',
+        descripcion: 'Pago y remuneracion Pago y remuneracion de hogar, bienestar y salud Pago y remuneracion',
+        origen: 'Direccion Gral de Administracion y Finanzas',
+        destino: 'Direccion Gral de Administracion y Finanzas',
+        dependenciaActual: 'Direccion Gral de Administracion y Finanzas',
+        estado: 'No recibido'
+      }
+    ]
+    const columns = [
+      {
+        name: 'ID',
+        selector: 'id',
+        sortable: true,
+        grow: -1
+      },
+      {
+        name: 'Número',
+        selector: 'numero',
+        grow: -1
+      },
+      {
+        name: 'Fecha Me',
+        selector: 'fechaMe',
+      },
+      {
+        name: 'Origen',
+        selector: 'origen',
+        wrap: true
+      },
+      {
+        name: 'Destino',
+        selector: 'destino',
+        wrap: true
+      },
+      {
+        name: 'Descripcion',
+        selector: 'descripcion',
+        grow: 2,
+        wrap: true
+      },
+      {
+        name: 'Estado',
+        selector: 'estado',
+        grow: -1
+      },
+      {
+        name: 'Dependencia Actual',
+        selector: 'dependenciaActual',
+        wrap: true
+      },
+      {
+        name: 'Acciones',
+        cell: () =>
+          <div>
+            <button className="btn btn-sm btn-link text-primary">
+              <FontAwesomeIcon icon="eye"/>
+            </button>
+            <button className="btn btn-sm btn-link text-primary">
+              <FontAwesomeIcon icon="print"/>
+            </button>
+          </div>,
+        button: true
+      }
+    ];
+
+    const paginationOptions = {
+      rowsPerPageText: 'Filas por página',
+      rangeSeparatorText: 'de',
+      selectAllRowsItem: true,
+      selectAllRowsItemText: 'Todos'
+    };
+
+    return (
+      //remplazar temp_data por this.state.list
       <DataTable
         columns={columns}
-        data={data}
+        data={this.state.list}
         pagination
         paginationComponentOptions={paginationOptions}
         highlightOnHover={true}
@@ -58,8 +104,8 @@ function Consulta(props) {
         dense={true}
         className="table-responsive table-sm table-bordered"
       />
-    </>
-  );
+    );
+  }
 }
 
 export default Consulta;
--- frontend/src/core/Consultas/Consultas.js
+++ frontend/src/core/Consultas/Consultas.js
@@ -1,66 +1,148 @@
-import React from "react";
+import React, {Component} from "react";
 import Consulta from "../../components/Tables/Consulta";
+import ConsultaService from "../../services/Consultas";
 import {Tabs, Tab} from "react-bootstrap";
 
-function Consultas() {
-  return (
-    <>
-      <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">Consultas</h1>
-      </div>
-      <Tabs defaultActiveKey="byId" id="uncontrolled-tab-example">
-        <Tab eventKey="byId" title="Buscar por ID">
-          <div className="col-6">
-            <h6 className="m-0 font-weight-bold col-form-label">Buscar Expediente</h6>
-            <div className="form-group row">
-              <label className="col-form-label col-sm-4">Número ID: </label>
-              <div className="col-5 input-group">
-                <input type="text" className="form-control-sm/"/>
-              </div>
-              <div className="col text-center">
-                <button className="btn btn-primary"> Buscar</button>
-              </div>
-            </div>
-          </div>
-        </Tab>
-        <Tab eventKey="byDescription" title="Buscar por descripción">
-          <div className="col-6">
-            <h6 className="m-0 font-weight-bold col-form-label">Buscar Expediente</h6>
-            <div className="form-group row">
-              <label className="col-form-label col-sm-4">Descripción: </label>
-              <div className="col-5 ">
-                <textarea className="form-control/"/>
-              </div>
-              <div className="col text-center">
-                <button className="btn btn-primary"> Buscar</button>
-            </div>
-          </div>
+class Consultas extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      id: '',
+      description: '',
+      num: '',
+      year: ''
+    }
+    this.handleIdChange = this.handleIdChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.handleNumChange = this.handleNumChange.bind(this);
+    this.handleYearChange = this.handleYearChange.bind(this);
+    this.findById = this.findById.bind(this);
+    this.handleIdSearch = this.handleIdSearch.bind(this);
+  }
+
+  handleIdChange = e => {
+    this.setState({id: e.target.value});
+  }
+
+  handleDescriptionChange = e => {
+    this.setState({description: e.target.value});
+  }
+
+  handleNumChange = e => {
+    this.setState({num: e.target.value});
+  }
+
+  handleYearChange = e => {
+    this.setState({year: e.target.value});
+  }
+
+  findById = id => {
+    //TODO verificar fechaMe y dependencia actual
+    ConsultaService.getById(id)
+      .then(response => {
+        this.setState({
+          data: [{
+            id: response.data.id,
+            numero: response.data.numero_mesa_de_entrada,
+            fechaMe: response.data.fecha_actualizacion,
+            origen: response.data.dependencia_origen_id.descripcion,
+            destino: response.data.dependencia_destino_id.descripcion,
+            descripcion: response.data.descripcion,
+            estado: response.data.estado_id.descripcion,
+            dependenciaActual: ''
+          }]
+        })
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  }
+
+  handleIdSearch = () => {
+    //check valid
+    this.findById(this.state.id);
+  }
+
+  render() {
+    return (
+      <>
+        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 className="h3 mb-0 text-gray-800">Consultas</h1>
         </div>
-        </Tab>
-        <Tab eventKey="byNumber" title="Buscar por número">
-          <div className="col-6">
-            <h6 className="m-0 font-weight-bold col-form-label">Buscar Expediente</h6>
-            <div className="form-group row">
-              <label className="col-form-label col-sm-4">Número de Expediente: </label>
-              <div className="col-5 input-group">
-                <input type="text" className="form-control-sm/"/>
+        <Tabs defaultActiveKey="byId" id="uncontrolled-tab-example">
+          <Tab eventKey="byId" title="Buscar por ID">
+            <div className="col-6">
+              <div className="form-group row">
+                <label className="col-form-label col-sm-4">Número ID: </label>
+                <div className="col-5 input-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    onChange={e => this.handleIdChange(e)}
+                    value={this.state.id}
+                  />
+                </div>
+                <div className="col text-center">
+                  <button
+                    className="btn btn-sm btn-primary"
+                    onClick={this.handleIdSearch}
+                  > Buscar</button>
+                </div>
               </div>
-              <div className="col text-center">
-                <button className="btn btn-primary"> Buscar</button>
             </div>
-          </div>
-          <div className="form-group row">
-            <label className="col-form-label col-sm-4">Año: </label>
-            <div className="col-5 input-group">
-              <input type="text" className="form-control-sm/"/>
+          </Tab>
+          <Tab eventKey="byDescription" title="Buscar por descripción">
+            <div className="col-6">
+              <div className="form-group row">
+                <label className="col-form-label col-sm-4">Descripción: </label>
+                <div className="col-5 ">
+                  <textarea
+                    className="form-control form-control-sm"
+                    onChange={e => this.handleDescriptionChange(e)}
+                    value={this.state.description}
+                  />
+                </div>
+                <div className="col text-center">
+                  <button className="btn btn-sm btn-primary"> Buscar</button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        </Tab>
-      </Tabs>
-      <Consulta/>
-    </>
-  );
+          </Tab>
+          <Tab eventKey="byNumber" title="Buscar por número">
+            <div className="col-6">
+              <div className="form-group row">
+                <label className="col-form-label col-sm-4">Número de Expediente: </label>
+                <div className="col-5 input-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    onChange={e => this.handleNumChange(e)}
+                    value={this.state.num}
+                  />
+                </div>
+                <div className="col text-center">
+                  <button className="btn btn-sm btn-primary"> Buscar</button>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-form-label col-sm-4">Año: </label>
+                <div className="col-5 input-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    onChange={e => this.handleYearChange(e)}
+                    value={this.state.year}
+                  />
+                </div>
+              </div>
+            </div>
+          </Tab>
+        </Tabs>
+        <Consulta data={this.state.data}/>
+      </>
+    );
+  }
 }
 
 export default Consultas;
